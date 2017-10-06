2dfb4e154a51c2c61870c94d352ca189ed7c69c8
slayercoder <ms81291@gmail.com>
2017-10-06 22:31:10 +0530

    Publishing nodejs articles

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-let yearElement = document.querySelector("#year");
-yearElement.innerHTML = (new Date()).getFullYear();
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    var feedTitle = document.querySelectorAll(".feedTitle");
+    
+    
+    // for NodeJS section
+    req = new XMLHttpRequest();
+    req.open("GET", "http://localhost:4000/feeds/archived/nodejs", true);
+    req.send();
+    req.onload = function(){
+        json = JSON.parse(req.response);
+        console.log(json);
+        for(var i = 0; i < 3; i++){
+            feedTitle[i].textContent = json[i].title;
+            feedTitle[i].
+        }
+    }     
+    
+    
+    
+    
+    
+    
+    
+    
+    let yearElement = document.querySelector("#year");
+    yearElement.innerHTML = (new Date()).getFullYear();
+});
+
+
+
+
+
