48a4c5a406c4c76c2e9a1123d12f27d5a59692a9
slayercoder <ms81291@gmail.com>
2017-10-08 12:58:59 +0530

    devops and nodejs publish UI rendering

--- js/script.js
+++ js/script.js
@@ -2,20 +2,35 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     var feedTitle = document.querySelectorAll(".feedTitle");
-    
+    var extLink = document.querySelectorAll(".extLink");
     
     // for NodeJS section
-    req = new XMLHttpRequest();
-    req.open("GET", "http://localhost:4000/feeds/archived/nodejs", true);
-    req.send();
-    req.onload = function(){
-        json = JSON.parse(req.response);
+    req1 = new XMLHttpRequest();
+    req1.open("GET", "http://localhost:4000/feeds/published/nodejs", true);
+    req1.send();
+    req1.onload = function(){
+        var json = JSON.parse(req1.response);
         console.log(json);
         for(var i = 0; i < 3; i++){
             feedTitle[i].textContent = json[i].title;
-            feedTitle[i].
+            extLink[i].setAttribute("href", json[i].link);
+
         }
     }     
+
+    // for Devops section
+
+    req2 = new XMLHttpRequest();
+    req2.open("GET", "http://localhost:4000/feeds/published/devops", true);
+    req2.send();
+    req2.onload = function(){
+        var json = JSON.parse(req2.response);
+        console.log(json);
+        for(var i = 3; i < 6; i++){
+            feedTitle[i].textContent = json[i - 3].title;
+            extLink[i].setAttribute("href", json[i - 3].link);
+        }
+    }
     
     
     
