b332195717b3bbdc23b5b05942710c98c00d7511
slayercoder <ms81291@gmail.com>
2017-10-11 02:34:15 +0530

    Devops and Nodejs detailed feed-pages added

--- js/script_devops.js
+++ js/script_devops.js
@@ -7,33 +7,40 @@ req.onload = function(){
     for(var i = 0; i < len; i++){
         if(len > 0){
             var cardDiv = document.createElement("div");
-            cardDiv.className = "col-md-4 col-sm-4 hero-feature";
+            cardDiv.setAttribute("class", "col-md-4 col-sm-4 hero-feature");
             
-            var cardHyperlink = cardDiv.appendChild(document.createElement("a"));
-            // cardHyperlink.setAttribute("class", "extLink");
+            var cardHyperlink = document.createElement("a");
+            cardHyperlink.setAttribute("class", "extLink");
             cardHyperlink.setAttribute("target", "_blank");
             cardHyperlink.setAttribute("href", json[i].link);
+            
             var thumbNail = document.createElement("div");
             thumbNail.setAttribute("class", "thumbnail thumbnail-jumbotron hero");
             
-            cardHyperlink.appendChild(thumbNail);
+            
             
             var cardImage = document.createElement("img");
             cardImage.setAttribute("src", "./images/nodeweekly.png");
             
-            thumbNail.appendChild(cardImage);
+            
             
             var caption = document.createElement("div");
             caption.setAttribute("class", "caption");
-            thumbNail.appendChild(caption);
+           
             
             var feedTitle = document.createElement("h3");
             feedTitle.setAttribute("class", "feedTitle");
             feedTitle.textContent = json[i].title;
-            caption.appendChild(feedTitle);
+            
             
             // appending to outer card container
             var cardContainer = document.querySelector(".cardContainer");
+            cardDiv.appendChild(cardHyperlink);
+            thumbNail.appendChild(cardImage);
+            thumbNail.appendChild(caption);
+            cardHyperlink.appendChild(thumbNail);
+
+            caption.appendChild(feedTitle);
             cardContainer.appendChild(cardDiv);
         }     
     }
